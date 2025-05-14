828aa83f9b01f5621a4aaaaf460d406e44a0605d
редкачева дарина <darinakaroche@mail.ru>
2025-05-14 16:23:16 +0700

    внесение некоторых изменений в диграмму и js-файл

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-Цзы',
+        name: 'Дао де цзин. Канок о Дао и Дэ',
+        publicationYear: 2022,
+        price: 300,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,  
+        price: 400,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 500,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Мария Корелли',
+        name: 'Скорбь Сатаны',
+        publicationYear: 2023,
+        price: 300,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Майк Омер',
+        name: 'За спиной',
+        publicationYear: 2025,  
+        price: 400,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Борис Васильев',
+        name: 'В списках не значился',
+        publicationYear: 1974,
+        price: 500,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Александр Твардовский',
+        name: 'Василий Теркин',
+        publicationYear: 1945,
+        price: 300,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Борис Васильев',
+        name: 'А зори здесь тихие',
+        publicationYear: 1969,  
+        price: 400,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Кэтрин Стокетт',
+        name: 'Прислуга',
+        publicationYear: 2009,
+        price: 500,
+        bestseller: false,
+        available: false
+    },
+        {
+        author: 'Патрик Зюскинд',
+        name: 'Парфюмер. История одного убийцы',
+        publicationYear: 1985,
+        price: 300,
+        bestseller: true,
+        available: false
+    },
+];
